6a88faced5498d07f14dc6a3fcdf53e059019160
Ferreira <Pedro.Ferreira@Dellteam.com>
2022-06-20 17:44:42 -0300

    Browsing pages with the router-link

---
+++ rotas-exercicios/src/components/template/Menu.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="menu">
+        <ul>
+            <router-link 
+                to="/" 
+                tag="li"
+                active-class="active" exact
+            >
+                <a>
+                    Inicio
+                </a>
+            </router-link>
+            <router-link 
+                to="/usuarios" 
+                tag="li"
+                active-class="active"
+            >
+                <a>
+                    Usuarios
+                </a>
+            </router-link>
+        </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
